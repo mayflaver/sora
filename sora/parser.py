b0a8f57d535eb88e0163ea49b5cc1ea3f6a1276b
Mayflower <fucongwang@gmail.com>
2015-02-13 11:46:54 +0800

    Add reset methed and Add unsizedParserBuffer

--- sora/parser.py
+++ sora/parser.py
@@ -31,3 +31,61 @@ class SizedParserBuffer(object):
     def reset(self):
         self.buffer = ''
         self.received = 0
+
+    def __eq__(self, other):
+        return self.buffer == other.buffer and self.received == other.received
+
+
+class UnsizedParserBuffer(object):
+    """ unsized parser buffer """
+    def __init__(self, terminal, include=False, skip=0):
+        self.terminal = terminal
+        self.include = include
+        self.skip = skip
+        self.data = ''
+        self.skiped = 0
+        self.check_index = 0
+
+    def _add_byte(self, byte):
+        if (self.skiped < self.skip):
+            self.skiped += 1
+            return False
+        
+        elif (byte == self.terminal[self.check_index]):
+            if (self.include):
+                self.data += byte
+            if (self.check_index == len(self.terminal)-1):
+                return True
+            else:
+                self.check_index += 1
+                return False
+        else:
+            if (self.check_index != 0):
+                if (not self.include):
+                    self.data += self.terminal[0:self.check_index]
+                self.check_index = 0
+            self.data += byte
+            return False
+
+    def add_data(self, data):
+        last = False
+        while (data.has_next and not last):
+            last = self._add_byte(data.next)
+        return last
+
+    @property
+    def result(self):
+        return self.data
+
+    def reset(self):
+        self.data = ''
+        self.skiped = 0
+        self.check_index = 0
+
+    def __eq__(self, other):
+        return self.terminal == other.terminal \
+            and self.include == other.include \
+            and self.skip == other.skip \
+            and self.data == other.data \
+            and self.skiped == other.skiped \
+            and self.check_index == other.check_index
--- tests/test_parser_buffer.py
+++ tests/test_parser_buffer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from sora.parser import SizedParserBuffer
+from sora.parser import SizedParserBuffer, UnsizedParserBuffer
 from sora.iobuffer import IOBuffer
 from nose.tools import assert_equal
 
@@ -27,7 +27,69 @@ class TestSizedParserBuffer(object):
         assert_equal(True, self.sizedParserBuffer.add_data(IOBuffer('lo')))
         assert_equal(True, self.sizedParserBuffer.is_finished)
 
+    def test_equal(self):
+        assert_equal(self.sizedParserBuffer, SizedParserBuffer(4))
+
     def test_reset(self):
         self.sizedParserBuffer.add_data(IOBuffer('hel'))
         self.sizedParserBuffer.reset()
-        assert_equal(0, self.sizedParserBuffer.received)
+        assert_equal(self.sizedParserBuffer, SizedParserBuffer(4))
+
+
+class TestUnsizedParserBufferUnincludeTerminal(object):
+
+    def setUp(self):
+        self.unsizedParserBufferUnincludeTerminal = UnsizedParserBuffer("foo")
+
+    def test_add_data(self):
+        assert_equal(False, self.unsizedParserBufferUnincludeTerminal.add_data(IOBuffer("somef")))
+        assert_equal(True, self.unsizedParserBufferUnincludeTerminal.add_data(IOBuffer("oos")))
+        assert_equal("some", self.unsizedParserBufferUnincludeTerminal.result)
+
+    def test_equal(self):
+        assert_equal(self.unsizedParserBufferUnincludeTerminal, UnsizedParserBuffer("foo"))
+
+    def test_reset(self):
+        self.unsizedParserBufferUnincludeTerminal.add_data(IOBuffer("somef"))
+        self.unsizedParserBufferUnincludeTerminal.reset()
+        assert_equal(self.unsizedParserBufferUnincludeTerminal, UnsizedParserBuffer("foo"))
+
+                     
+
+        
+class TestUnsizedParserBufferIncludeTerminal(object):
+    
+    def setUp(self):
+        self.unsizedParserBufferIncludeTerminal = UnsizedParserBuffer("foo", True)
+        
+    def test_add_data(self):
+        assert_equal(False, self.unsizedParserBufferIncludeTerminal.add_data(IOBuffer("somef")))
+        assert_equal(True, self.unsizedParserBufferIncludeTerminal.add_data(IOBuffer("oos")))
+        assert_equal("somefoo", self.unsizedParserBufferIncludeTerminal.result)
+
+    def test_equal(self):
+        assert_equal(self.unsizedParserBufferIncludeTerminal, UnsizedParserBuffer("foo", True))
+
+    def test_reset(self):
+        self.unsizedParserBufferIncludeTerminal.add_data(IOBuffer("somef"))
+        self.unsizedParserBufferIncludeTerminal.reset()
+        assert_equal(self.unsizedParserBufferIncludeTerminal, UnsizedParserBuffer("foo", True))
+
+
+class TestUnsizedParserBufferSkiped(object):
+    
+    def setUp(self):
+        self.unsizedParserBufferSkiped = UnsizedParserBuffer("foo", False, 2)
+
+    def test_add_data(self):
+        assert_equal(False, self.unsizedParserBufferSkiped.add_data(IOBuffer("somef")))
+        assert_equal(True, self.unsizedParserBufferSkiped.add_data(IOBuffer("oos")))
+        assert_equal("me", self.unsizedParserBufferSkiped.result)
+
+    def test_equal(self):
+        assert_equal(self.unsizedParserBufferSkiped, UnsizedParserBuffer("foo", False, 2))
+
+    def test_reset(self):
+        self.unsizedParserBufferSkiped.add_data(IOBuffer("somef"))
+        self.unsizedParserBufferSkiped.reset()
+        assert_equal(self.unsizedParserBufferSkiped, UnsizedParserBuffer("foo", False, 2))
