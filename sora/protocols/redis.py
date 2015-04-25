d53ebd55bfeec09cbac4b2f14e8812b04fddf03c
Mayflower <fucongwang@gmail.com>
2015-04-25 22:26:09 +0800

    Add redis protocol support

---
+++ sora/protocols/redis.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from sora.parser import Byte, Bytes, BytesUntil
+
+def simple_strings_parser():
+    return BytesUntil('\r\n')
+
+def errors_parser():
+    return BytesUntil('\r\n')
+
+def integers_parser():
+    return BytesUntil('\r\n').then(lambda n: int(n))
+
+def bulk_strings_parser():
+    return BytesUntil('\r\n').link(lambda x: Bytes(int(x)).precombine(Bytes(2)))
+
+def arrays_parser():
+    def help(n):
+        parser = command_parser()
+        if n == 1:
+            parser = command_parser().then(lambda x: (x, ))
+        if n > 1:
+            for i in range(n-1):
+                parser = parser.combine(command_parser())
+        return parser
+    return BytesUntil('\r\n').link(lambda n: help(int(n)))
+
+def command_parser():
+    def help(c):
+        if c == '+':
+            return simple_strings_parser()
+        elif c == '-':
+            return errors_parser()
+        elif c == ':':
+            return integers_parser()
+        elif c == '$':
+            return bulk_strings_parser()
+        else:
+            return arrays_parser()
+    return Byte().link(lambda c: help(c))
---
+++ tests/protocols/test_redis.py
@@ -0,0 +1,35 @@
+from sora.iobuffer import IOBuffer
+from sora.protocols.redis import command_parser
+from nose.tools import assert_equal
+
+class TestRedis(object):
+
+    def setUp(self):
+        self.parser = command_parser()
+
+    def test_simple_strings_parser(self):
+        data = IOBuffer('+OK\r\n')
+        assert_equal('OK', self.parser.parser(data))
+
+    def test_errors_parser(self):
+        data = IOBuffer('-Error message\r\n')
+        assert_equal('Error message', self.parser.parser(data))
+
+    def test_integers_parser(self):
+        data = IOBuffer(':0\r\n')
+        assert_equal(0, self.parser.parser(data))
+
+    def test_bulk_strings_parser(self):
+        data = IOBuffer('$6\r\nfoobar\r\n')
+        assert_equal('foobar', self.parser.parser(data))
+
+    def test_array_string_parser(self):
+        # empty array
+        data0 = IOBuffer('*0\r\n')
+        assert_equal(None, self.parser.parser(data0))
+        # array with 1 element
+        data1 = IOBuffer('*1\r\n$3\r\nfoo\r\n')
+        assert_equal(('foo',), self.parser.parser(data1))
+        # array with >1 element
+        data2 = IOBuffer('*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n')
+        assert_equal(('foo', 'bar'), self.parser.parser(data2))
